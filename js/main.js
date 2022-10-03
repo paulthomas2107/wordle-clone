2f7bcac00bd3027df43d965a8c3016652f6a5862
Paul Thomas <paulthomas2107@gmail.com>
2022-10-03 13:17:03 +0100

    Allow letter accept

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   createSquares();
 
+  let guessedWords = [[]];
+  let availableSpace = 1;
+
+  const keys = document.querySelectorAll('.keyboard-row button');
+
+  for (let index = 0; index < keys.length; index++) {
+    keys[index].onclick = ({ target }) => {
+      const letter = target.getAttribute('data-key');
+      updateGuessedWords(letter);
+    };
+  }
+
+  function getCurrentWordArray() {
+    const numberOfGuessedWords = guessedWords.length;
+    return guessedWords[numberOfGuessedWords - 1];
+  }
+
+  function updateGuessedWords(letter) {
+    const currentWordArray = getCurrentWordArray();
+    if (currentWordArray && currentWordArray.length < 5) {
+      currentWordArray.push(letter);
+      const availableSpaceEl = document.getElementById(String(1));
+      availableSpace = availableSpace + 1;
+      availableSpaceEl.textContent = letter;
+    }
+  }
+
   function createSquares() {
     const gameBoard = document.getElementById('board');
 
